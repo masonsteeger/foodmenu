8adef544f739b362c18c837f9a837808b367f0c3
masonsteeger94 <masonsteeger@gmail.com>
2020-09-30 14:21:40 -0500

    adding create form in app.js

--- models/food_seed.js
+++ models/food_seed.js
@@ -3,6 +3,7 @@ module.exports = [
         _id: "5f74b3befef69b28e7e28b39",
         name: "Cheesy Garlic Shrimp Alfredo",
         course: "Entree",
+        price: 15,
         image: "https://spicysouthernkitchen.com/wp-content/uploads/Cheesy-Garlic-Shrimp-Alfredo-5.jpg",
         description: "A cheesy creamy pasta with delicious shrimp and garlic",
         "__v": 0
@@ -11,6 +12,7 @@ module.exports = [
         _id: "5f74b414fef69b28e7e28b3a",
         name: "Fried Mushrooms",
         course: "Appetizer",
+        price: 15,
         image: "https://i2.wp.com/www.angsarap.net/wp-content/uploads/2019/11/Deep-Fried-Mushrooms.jpg?w=1080&ssl=1",
         description: "Deep Fried Mushrooms is a simple side dish of button mushrooms breaded and deep fried served with a creamy onion dip",
         "__v": 0
@@ -19,6 +21,7 @@ module.exports = [
         _id: "5f74b4173a0222e765fde4e0",
         name: "Beef Wellington",
         course: "Entree",
+        price: 15,
         image: "https://static01.nyt.com/images/2019/12/13/dining/mc-beef-wellington/mc-beef-wellington-articleLarge.jpg",
         "description": "Beef fillet, blanketed with browned mushrooms and shallots. Breaded",
         "__v": 0
@@ -27,6 +30,7 @@ module.exports = [
         _id: "5f74b5103a0222e765fde4e1",
         name: "Charred Broccoli",
         course: "Appetizer",
+        price: 15,
         image: "https://www.fifteenspatulas.com/wp-content/uploads/2018/07/Charred-Broccoli-Fifteen-Spatulas-2-640x427.jpg",
         description: "Charred Broccoli side dish is simple to make, and so delicious. Broccoli florets are charred and tossed with fresh lemon, toasted pine nuts, tangy pecorino cheese, and fresh basil.",
         "__v": 0
@@ -35,6 +39,7 @@ module.exports = [
         _id: "5f74b531fef69b28e7e28b3b",
         name: "Triple Chocolate Cheesecake",
         course: "Dessert",
+        price: 15,
         image: "https://i0.wp.com/cookiedoughandovenmitt.com/wp-content/uploads/2017/05/triple-chocolate-cheesecake-photo.jpg",
         "description": "Chocolate oreo crust, a rich chocolate cheesecake filling, and a milk chocolate ganache topping.",
         "__v": 0
@@ -43,6 +48,7 @@ module.exports = [
         _id: "5f74b62b3a0222e765fde4e2",
         name: "S'more Station",
         course: "Dessert",
+        price: 15,
         image: "https://dmn-dallas-news-prod.cdn.arcpublishing.com/resizer/u46eXDnTo2OxOHe5PrwCEgYBjzA=/1660x0/smart/filters:no_upscale()/arc-anglerfish-arc2-prod-dmn.s3.amazonaws.com/public/RZ6EW5IHGQLN6H7TVW23TSHTOI.jpg",
         description: "The dessert comes with house-made maple syrup and cotton candy-flavored mallows, all the necessary cracker and chocolate accoutrements, and an open flame, leaving you to roast to perfection.",
         "__v": 0
